7916315b267be2a00e1ee5bcc747658538829def
“Gracie <gracie.sharma@renegadeinsurance.com>
2023-04-07 16:12:50 +0545

    expense delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+
+  const[expenses,setExpenses]=useState([
+    {id:1, description:'aaa', amount:10,category:'Utilities'},
+    {id:2, description:'abc', amount:10,category:'Utilities'},
+    {id:3, description:'hhjh', amount:10,category:'Utilities'},
+    {id:4, description:'nhjh', amount:10,category:'Utilities'},
+
+  ])
   return (
     <div>
-     <Form/>
-    </div>
+<ExpenseList expenses={expenses} onDelete={(id)=> setExpenses(expenses.filter(e => e.id !== id))}/>  
+  </div>
   );
 }
 export default App;
