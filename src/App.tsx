01f06475dca6f4523cad0944728c2928ee4df0b2
“Gracie <gracie.sharma@renegadeinsurance.com>
2023-05-30 20:03:28 +0545

    effect dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
 
-function App() {
+const App = () => {
+  const [category, setCategory] = useState("");
   const ref = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (ref.current) ref.current.focus();
-  });
 
   useEffect(() => {
-    document.title = "My document";
-  });
+    if (ref.current) {
+      ref.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = "My App";
+  }, []);
+
   return (
     <div>
-      <select name="" id="" className="form-select">
-        <option value=""></option>
-        <option value="clothing">Clothing</option>
-        <option value="household">Household </option>
-        <option value=""></option>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value="value1"></option>
+        <option value="swimwear">Swimwear</option>
+        <option value="shoe">Shoe</option>
       </select>
-
-      <ProductList />
+      <ProductList category={category} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-const ProductList = () => {
+const ProductList = ({ category }: { category: string }) => {
   const [products, setProducts] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log("Fetching Products ");
-    setProducts(["Clothing", "Swimwears"]);
-  },[]); 
+    console.log('Fetching products in : ', category);
+    setProducts(['swimwear', 'shoe' ]);
+  }, [category]);
 
   return <div>ProductList</div>;
 };
